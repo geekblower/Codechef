8d7c79e534c624f95611e7c3d5963562bf867fd9
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-06-24 23:57:52 +0530

    Update STFOOD.cpp

--- DSA Learning Series/03. Linear Data Structures (LRNDSA02)/STFOOD.cpp
+++ DSA Learning Series/03. Linear Data Structures (LRNDSA02)/STFOOD.cpp
@@ -1,8 +1,5 @@
-/*
-    Author - Akash Gautam (@geekblower)
-    Date - 24-06-2022
-    Platform - Codechef
-*/
+//Author: Akash Gautam (@geekblower)
+//Date: 24-06-2022
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -14,13 +11,13 @@ using namespace std;
 #define VI vector<int>
 #define PB push_back
 #define ALL(a) a.begin(),a.end()
-#define SORT(a) sort(a.begin(),a.end())
+#define SORT(a) sort(ALL(a))
 #define loop(i,a,b,c) for(int i=a;i<b;i+=c)
 #define revloop(i,a,b,c) for(int i=a;i<b;i-=c)
 #define start_loop loop(i,0,n,1)
 #define start_revloop revloop(i,0,n,1)
 #define create(n) int n;cin>>n;
-#define def(a,n) inta[n];loop(i,0,n){cin>>a[i];}
+#define def(a,n) int a[n];start_loop cin>>a[i];
 #define print(a) for(auto i : a){cout<<i<<" ";}cout<<endl;
 const int MOD = 1e9+7;
 
